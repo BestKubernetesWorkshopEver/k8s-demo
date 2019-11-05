edb29c16a59b01432c070ee73127ec7ad722e613
Wojciech Wojcik <wojciech.wojcik@harman.com>
2019-11-05 14:45:16 +0000

    adding allert and user password frmo ui

--- frontend-service/frontend/src/app/app.component.ts
+++ frontend-service/frontend/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Component({
@@ -8,13 +8,36 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 })
 export class AppComponent {
   rockets: Rocket[] = [];
+  user: string;
+  password: string;
 
-  constructor(
+  @ViewChild('alert', { static: true }) alert: ElementRef;
+
+    constructor(
     private http: HttpClient){}
 
   startRocket(): void {
     console.log('Starting new rocker ...')
-    this.http.get<Rocket[]>('api').subscribe(rockets => {this.rockets=rockets.reverse();});
+    console.log(this.user+' '+this.password)
+
+    let headers = new HttpHeaders();
+    headers = headers.append("Authorization", "Basic " + btoa(this.user+':'+this.password));
+    const httpOptions = {
+      headers: headers
+      }
+    
+    this.alert.nativeElement.classList.remove('show');
+    this.http.get<Rocket[]>('api', httpOptions).subscribe(
+      rockets => {
+                this.rockets=rockets.reverse();
+                }, 
+      error => { console.log('Error happend');
+                this.alert.nativeElement.classList.add('show');
+                });
+  }
+
+  closeAlert() {
+    this.alert.nativeElement.classList.remove('show');
   }
 }
 
